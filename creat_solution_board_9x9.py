b10d2b2b5b618a9cbb9929200516f9fa1e31967b
구민재 <koom0406@naver.com>
2020-12-17 22:43:56 +0900

    정답보드 완성

---
+++ creat_solution_board_9x9.py
@@ -0,0 +1,7 @@
+def create_solution_board_9x9():
+    board = initialize_board_9x9()
+    board = shuffle_ribbons(board)
+    board = transpose(board)
+    board = shuffle_ribbons(board)
+    board = transpose(board)
+    return board
