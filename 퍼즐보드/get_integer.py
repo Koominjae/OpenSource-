1dc69007ac71acd631985dd5083d3544748dca96
구민재 <koom0406@naver.com>
2020-12-18 13:56:38 +0900

    숫자 입력받기

--- 퍼즐보드/get_integer.py
+++ 퍼즐보드/get_integer.py
@@ -1,5 +1,5 @@
 def get_integer(message, i ,j):
     number = input(message)
-    while not (number.isdigit() and i <= int(number) <= j):
+    while not (number.isdigit() and i <= int(number) <= j): 
         number = input(message)
     return int(number)
