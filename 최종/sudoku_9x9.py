c8f3e571eae40fa299fcd01b72f08d35e170da0d
구민재 <koom0406@naver.com>
2020-12-18 14:01:45 +0900

    최종

--- 최종/sudoku_9x9.py
+++ 최종/sudoku_9x9.py
@@ -82,7 +82,7 @@ def show_board(board):
         print()
         i += 1
 
-def get_integer(message, i ,j):
+def get_integer(message, i ,j): 
     number = input(message)
     while not (number.isdigit() and i <= int(number) <= j):
         number = input(message)
